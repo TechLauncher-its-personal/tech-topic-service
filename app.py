22146381af6f0b1bfda430c3185207b15fb67c79
Anggriosutopo <u7162593@anu.edu.au>
2021-10-24 21:06:22 +0700

    Add feature to check for existing articles

--- app.py
+++ app.py
@@ -8,47 +8,65 @@ import psycopg2
 @get('/test')
 def get_test():
     # Gives the 5 top stories from CNN, BBC, ABC, NYTimes, and Fox News to test article output
+    # title, summary, link, published
     cnnRss = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
-    cnnEntries = cnnRss["entries"] # title, summary, link, published
+    cnnEntries = cnnRss["entries"]
     bbcRss = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
-    bbcEntries = bbcRss["entries"] # title, summary, link, published
+    bbcEntries = bbcRss["entries"] 
     abcRss = feedparser.parse("https://www.abc.net.au/news/feed/45910/rss.xml")
-    abcEntries = abcRss["entries"] # title, summary, link, published
+    abcEntries = abcRss["entries"] 
     nytimesRss = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
-    nytimesEntries = nytimesRss["entries"] # title, summary, link, published
+    nytimesEntries = nytimesRss["entries"] 
     foxnewsRss = feedparser.parse("http://feeds.foxnews.com/foxnews/latest")
-    foxnewsEntries = foxnewsRss["entries"] # title, summary, link, published
+    foxnewsEntries = foxnewsRss["entries"] 
     myData = cnnEntries[:1] + bbcEntries[:1] + abcEntries[:1] + nytimesEntries[:1] + foxnewsEntries[:1]
     return {"data":myData}
 
 @get('/random')
 def get_random():
+    # Get the combination of the top 5 stories from CNN, BBC, ABC, NYTimes, and Fox News
+    # title, summary, link, published
     cnnRss = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
-    cnnEntries = cnnRss["entries"] # title, summary, link, published
+    cnnEntries = cnnRss["entries"] 
     bbcRss = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
-    bbcEntries = bbcRss["entries"] # title, summary, link, published
+    bbcEntries = bbcRss["entries"] 
     abcRss = feedparser.parse("https://www.abc.net.au/news/feed/45910/rss.xml")
-    abcEntries = abcRss["entries"] # title, summary, link, published
+    abcEntries = abcRss["entries"] 
     nytimesRss = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
-    nytimesEntries = nytimesRss["entries"] # title, summary, link, published
+    nytimesEntries = nytimesRss["entries"] 
     foxnewsRss = feedparser.parse("http://feeds.foxnews.com/foxnews/latest")
-    foxnewsEntries = foxnewsRss["entries"] # title, summary, link, published
+    foxnewsEntries = foxnewsRss["entries"] 
     myData = cnnEntries[:5] + bbcEntries[:5] + abcEntries[:5] + nytimesEntries[:5] + foxnewsEntries[:5]
+    
+    # Get the current database values
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    sql = ("SELECT title FROM tech")
+    cur.execute(sql)
+    database = [row[0] for row in cur.fetchall()]
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    # Remove the existing articles from the list of new top stories
+    myData = [article for article in myData if article["title"] not in database]
     return {"data":random.choice(myData)}
 
 @get('/tech')
 def get_news_article():
-    # TODO: Check articles until one passes the test for a certain topic, then return that article
+    # Check articles until one passes the test for a certain topic, then return that article
+    # title, summary, link, published
     cnnRss = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
-    cnnEntries = cnnRss["entries"] # title, summary, link, published
+    cnnEntries = cnnRss["entries"] 
     bbcRss = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
-    bbcEntries = bbcRss["entries"] # title, summary, link, published
+    bbcEntries = bbcRss["entries"] 
     abcRss = feedparser.parse("https://www.abc.net.au/news/feed/45910/rss.xml")
-    abcEntries = abcRss["entries"] # title, summary, link, published
+    abcEntries = abcRss["entries"] 
     nytimesRss = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
-    nytimesEntries = nytimesRss["entries"] # title, summary, link, published
+    nytimesEntries = nytimesRss["entries"] 
     foxnewsRss = feedparser.parse("http://feeds.foxnews.com/foxnews/latest")
-    foxnewsEntries = foxnewsRss["entries"] # title, summary, link, published
+    foxnewsEntries = foxnewsRss["entries"] 
     nlp = spacy.load("training/model-best")
     entries = cnnEntries + bbcEntries + abcEntries + nytimesEntries + foxnewsEntries
     random.shuffle(entries)
@@ -69,7 +87,7 @@ def get_news_article():
 
 @get('/training/tech/<title>/<relevant>')
 def get_training(title, relevant):
-    # TODO: Add article to training data
+    # Add article to the training data database
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
